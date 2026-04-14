7441b8a357ebb73bb195b660ef73cdc0d5aba73f
Yogesh <yogeshjaatjaat4343@gmail.com>
2026-04-14 10:15:50 +0530

    server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+let users = {};
+
+app.get("/", (req,res)=>{
+    res.send("Backend running");
+});
+
+app.get("/user/:id", (req,res)=>{
+    res.json({ coins: users[req.params.id] || 0 });
+});
+
+app.get("/callback", (req,res)=>{
+    let user = req.query.user_id;
+    let reward = parseFloat(req.query.reward) || 0;
+
+    let coins = Math.floor(reward * 100 * 0.5);
+
+    if(!users[user]) users[user] = 0;
+
+    users[user] += coins;
+
+    res.send("OK");
+});
+
+app.listen(process.env.PORT || 3000);
